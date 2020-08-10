ef357b2c71abd3052deea69c2fa815a1ce6daeaa
liangguojiang <657347473@qq.com>
2020-08-10 17:23:26 +0800

    组件管理完成

--- src/views/custom/Panel/componentsPanel.vue
+++ src/views/custom/Panel/componentsPanel.vue
@@ -1,19 +1,35 @@
 <template>
   <div class="components-panel">
     <panel-header title="组件管理" />
+    <draggable :list="pages" v-bind="{ animation: 300 }">
+      <div v-for="(item, index) in pages" :key="index" class="component">
+        {{ item.name }}组件
+      </div>
+    </draggable>
   </div>
 </template>
 
 <script>
 import panelHeader from '../common/panelHeader'
+import draggable from 'vuedraggable'
 export default {
-  components: { panelHeader },
-  data() {
-    return {}
-  },
-  mounted() {},
-  created() {},
-  methods: {}
+  components: { panelHeader, draggable },
+  computed: {
+    pages() {
+      return this.$store.state.custom.pages
+    }
+  }
 }
 </script>
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.components-panel {
+  .component {
+    height: 40px;
+    padding-left: 15px;
+    line-height: 40px;
+    font-size: 14px;
+    cursor: move;
+    border-bottom: 1px solid #eee;
+  }
+}
+</style>
